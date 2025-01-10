9e491ff5d2fb322f2918e94ebc7ed8f9bf795175
Sam <samuelxborges@gmail.com>
2025-01-09 22:01:35 -0500

    Card update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,64 +13,66 @@ type card = {
 
 const cards: card[] = [
     {
-        title: 'yo mama.net', 
-        subtitle: 'butt',
+        title: 'Kmodo', 
+        subtitle: 'January 2025 - Present',
         image: 'logos/github-mark.svg',
-        body: 'lalalalalalalalala',
-        skills: ['penis', 'balls', 'dylan vidal', 'kmodo'],
+        body: 'lalalalalalalalala ijij ijijoiudsifn ijsiudfniuwejf ijosenfbn ijsoidjfonion slkhgseufghbskehrgouayhergi',
+        skills: ['k', 'm', 'o', 'd', 'o '],
         link: 'https://www.dvidal.me/'
     },
 
     {
-        title: 'yo mama.nett', 
-        subtitle: 'buttt',
-        image: 'logos/github-mark.svg',
+        title: 'Spark-a-Hack', 
+        subtitle: 'KnightHacks VII 2024',
+        image: '/logos/sparkahack.png',
         body: 'lalalalalalalalala',
-        skills: ['penis', 'balls', 'dylan vidal', 'kmodo'],
+        skills: ['React', 'Tailwind CSS', 'Next.js', 'TypeScript', 'tRPC', 'Gemini API', ],
         link: 'https://www.dvidal.me/'
     },
 
     {
-        title: 'yo mama.nettt', 
-        subtitle: 'butttt',
+        title: 'Night of Knights', 
+        subtitle: 'May 2023 – May 2024',
         image: 'logos/github-mark.svg',
         body: 'lalalalalalalalala',
-        skills: ['penis', 'balls', 'dylan vidal', 'kmodo'],
-        link: 'https://www.dvidal.me/'
+        skills: ['C#', 'Unity'],
+        link: 'https://play.unity.com/en/games/27ee33fa-e1e3-4178-8acf-9e1f37546fd0/night-of-knights'
     },
 
-    {
-        title: 'yo mama.netttt', 
-        subtitle: 'buttttt',
-        image: 'logos/github-mark.svg',
-        body: 'lalalalalalalalala',
-        skills: ['penis', 'balls', 'dylan vidal', 'kmodo'],
-        link: 'https://www.dvidal.me/'
-    },
 ];
 
 const Card = () => {
 
     return(
         <div className="min-h-screen flex flex-col items-center justify-center pt-16 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-10 w-full'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 w-full'>
             {
                 cards.map(item => {
                     return (
-                        <div key={item.title} className='menu-item m-1 sm:m-2 md:m-3 lg:m-4 xl:m-5 text-[#F5F1ED] bg-[#1B4465] text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl p-10 rounded-3xl transform transition hover:scale-110'>
+                        <div key={item.title} className='menu-item w-auto text-[#F5F1ED] bg-[#1B4465] p-10 rounded-3xl transform transition sm:scale-90 hover:scale-100 border-solid border-[#F5F1ED] border-4 shadow-xl shadow-transparent hover:shadow-[#F5F1ED]'>
                                 <div className='flex flex-col'>
-                                    <div className='flex flex-row items-center justify-left'>
-                                        <div className='pr-5'><Image src={item.image} alt={item.title} title={item.title} width={30} height={30}></Image></div>
+                                    <div className='flex flex-row items-center justify-left pb-5'>
+                                        <div className='pr-5'><Image src={item.image} alt={item.title} title={item.title} width={50} height={50} /></div>
                                         <div className='flex flex-col justify-left'>
-                                            <div className='text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
+                                        <Link href={item.link ?? '#'} target='_blank' rel="noopener noreferrer">
+                                            <div className='font-bold text-md sm:text-base md:text-lg lg:text-xl xl:text-2xl hover:underline transform transition hover:scale-110'>
                                                 {item.title}
                                             </div> 
-                                            <div>{item.subtitle}</div> 
+                                        </Link>
+                                            <div className='font-semibold text-sm sm:text-xs md:text-base lg:text-base xl:text-lg'>
+                                                {item.subtitle}
+                                            </div> 
                                         </div>
                                     </div>
                                     
-                                    <div>{item.body}</div> <div>{item.skills}</div>
-                                    <Link href={item.link} target='_blank' rel="noopener noreferrer">{item.link}</Link>
+                                    <div className='pb-5'>{item.body}</div> 
+
+                                    <div className='flex flex-wrap gap-2'>
+                                        {item.skills.map(skill => (
+                                            <span key={skill} className='bg-[#13293D] p-2 rounded-md'>{skill}</span>
+                                        ))}
+                                    </div>
+                                    
                                 </div>
                         </div>
                     )
